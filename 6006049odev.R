e872662a944ea248164bb4bf831f341fc5d7ae28
= <=>
2020-10-20 21:38:00 +0300

    Splitting the dataset into the Training set and Test set

--- 6006049odev.R
+++ 6006049odev.R
@@ -4,3 +4,11 @@
 #install.packages("readxl")
 library("readxl")
 dataset = read_excel('loandeneme.xlsx', sheet="bankcustomer")
+
+#Splitting the dataset into the Training set and Test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$LoanAmountinK, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
